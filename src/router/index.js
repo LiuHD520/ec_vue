16178b081e88fa61cd24a44eaf7cecda538cc3cd
hedong <1032015364@qq.com>
2018-07-22 21:45:33 +0800

    跳转

--- src/router/index.js
+++ src/router/index.js
@@ -15,5 +15,8 @@ export default new Router({
     { path: '/login', name: 'Login', component: Login },
     { path: '/goods', name: 'Goods', component: Goods },
     { path: '/categoryList', name: 'CategoryList', component: CategoryList },
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 });
